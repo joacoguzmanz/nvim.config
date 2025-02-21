d53fb70b100e48382627ddc54426f506092f1f08
Joaquin Guzman <joaquingusman77@gmail.com>
2025-02-21 23:41:37 +0100

    upload configs

---
+++ lua/joacoguzman/lazy/none-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.erb_lint,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.black,
+      },
+    })
+  end,
+}
