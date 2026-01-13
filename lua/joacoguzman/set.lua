dc3eb15877926e520acb78df88b279753a86fc0b
Joaco Guzman <joaquingusman77@gmail.com>
2026-01-13 21:45:42 +0100

    Add comments for configuration clarity in set.lua
    Added comments to clarify settings in set.lua.

--- lua/joacoguzman/set.lua
+++ lua/joacoguzman/set.lua
@@ -1,27 +1,34 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+-- Line numbers
+vim.opt.nu = true                 -- show absolute line numbers
+vim.opt.relativenumber = true     -- relative numbers from cursor
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+-- Tabs & indentation
+vim.opt.tabstop = 2               -- visual width of a tab
+vim.opt.softtabstop = 2           -- spaces inserted/removed with Tab/BS
+vim.opt.shiftwidth = 2            -- spaces used for auto-indent
+vim.opt.expandtab = true          -- convert tabs to spaces
+vim.opt.smartindent = true        -- smart auto-indentation
 
-vim.opt.smartindent = true
+-- Wrapping
+vim.opt.wrap = false              -- do not wrap long lines
 
-vim.opt.wrap = false
+-- Files
+vim.opt.swapfile = false          -- disable swap files
+vim.opt.backup = false            -- disable backup files
 
-vim.opt.swapfile = false
-vim.opt.backup = false
+-- Search
+vim.opt.hlsearch = false          -- no persistent search highlight
+vim.opt.incsearch = true          -- incremental search highlight
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- UI
+vim.opt.termguicolors = true      -- enable 24-bit colours
+vim.opt.scrolloff = 8             -- keep 8 lines above/below cursor
+vim.opt.signcolumn = "yes"        -- always show sign column
+vim.opt.isfname:append("@-@")     -- allow '@' in file names
 
-vim.opt.termguicolors = true
+-- netrw (built-in file explorer)
+vim.g.netrw_browse_split = 0      -- open files in same window
+vim.g.netrw_banner = 0            -- hide netrw banner
+vim.g.netrw_winsize = 25          -- netrw split size (if used)
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
--- vim.opt.colorcolumn = "80"
-
-vim.opt.scrolloff = 8
+-- vim.opt.colorcolumn = "80"     -- show column guide (optional)
