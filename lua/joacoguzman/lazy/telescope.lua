de1151fbf49db094af176dba5a4ed1113150f219
Joaco Guzman <joaquingusman77@gmail.com>
2026-01-14 00:44:16 +0100

    Refactor telescope.lua configuration and keymaps

--- lua/joacoguzman/lazy/telescope.lua
+++ lua/joacoguzman/lazy/telescope.lua
@@ -1,29 +1,28 @@
 return {
     "nvim-telescope/telescope.nvim",
-
     tag = "0.1.8",
-
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+    dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
-        require('telescope').setup({})
+        require("telescope").setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-        vim.keymap.set('n', '<leader>pws', function()
+        local builtin = require("telescope.builtin")
+
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "find files" }) -- find files
+        vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "git files" })        -- find git files
+
+        vim.keymap.set("n", "<leader>pws", function()                                  -- grep word under cursor
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>pWs', function()
+
+        vim.keymap.set("n", "<leader>pWs", function()                                  -- grep WORD under cursor
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
-        end)
-    end
-}
 
+        vim.keymap.set("n", "<leader>ps", function()                                   -- grep prompt
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+    end,
+}
