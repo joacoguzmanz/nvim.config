954938e4c08b2491814872e3058e650c989ea196
Joaco Guzman <joaquingusman77@gmail.com>
2026-01-13 23:26:45 +0100

    Refactor LuaSnip configuration and key mappings

--- lua/joacoguzman/lazy/snippets.lua
+++ lua/joacoguzman/lazy/snippets.lua
@@ -1,29 +1,23 @@
 return {
     {
         "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
+        version = "v2.*",
         build = "make install_jsregexp",
-
         dependencies = { "rafamadriz/friendly-snippets" },
 
         config = function()
             local ls = require("luasnip")
-            ls.filetype_extend("javascript", { "jsdoc" })
 
-            --- TODO: What is expand?
-            vim.keymap.set({"i"}, "<C-s>e", function() ls.expand() end, {silent = true})
+            ls.filetype_extend("javascript", { "jsdoc" }) -- enable JSDoc snippets in JS
 
-            vim.keymap.set({"i", "s"}, "<C-s>;", function() ls.jump(1) end, {silent = true})
-            vim.keymap.set({"i", "s"}, "<C-s>,", function() ls.jump(-1) end, {silent = true})
-
-            vim.keymap.set({"i", "s"}, "<C-E>", function()
+            vim.keymap.set("i", "<C-l>", function() ls.expand() end, { silent = true })           -- expand snippet
+            vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, { silent = true })   -- next placeholder
+            vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(-1) end, { silent = true })  -- previous placeholder
+            vim.keymap.set({ "i", "s" }, "<C-e>", function()                                      -- cycle snippet choice
                 if ls.choice_active() then
                     ls.change_choice(1)
                 end
-            end, {silent = true})
+            end, { silent = true })
         end,
     }
 }
-
