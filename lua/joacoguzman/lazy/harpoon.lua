0b0f623b485ceb4cfd3b139ed9c2fad2878285ed
Joaco Guzman <joaquingusman77@gmail.com>
2026-01-13 22:36:04 +0100

    Add comments to Harpoon key mappings for clarity

--- lua/joacoguzman/lazy/harpoon.lua
+++ lua/joacoguzman/lazy/harpoon.lua
@@ -9,16 +9,16 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end) -- add file to Harpoon
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end) -- Harpoon quick menu
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end) -- go to Harpoon file 1
+        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end) -- go to Harpoon file 2
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end) -- go to Harpoon file 3
+        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end) -- go to Harpoon file 4
 
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end) -- previous Harpoon file
+        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end) -- next Harpoon file
 
         local conf = require("telescope.config").values
         local function toggle_telescope(harpoon_files)
@@ -39,10 +39,10 @@ return {
 
         vim.keymap.set("n", "<leader>te", function()
             toggle_telescope(harpoon:list())
-        end, { desc = "Open Harpoon Telescope picker" })
+        end, { desc = "Open Harpoon Telescope picker" }) -- Harpoon picker in Telescope
 
         vim.keymap.set("n", "<leader>hr", function()
-            require("harpoon.mark").rm_file()
-        end, { desc = "Remove current file from Harpoon" })
+            harpoon:list():remove()
+        end, { desc = "Remove current file from Harpoon" }) -- remove current file from Harpoon
     end
 }
