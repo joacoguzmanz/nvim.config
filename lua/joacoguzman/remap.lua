a2feab6e8183866c3ca24406b82ad28083502664
Joaco Guzman <joaquingusman77@gmail.com>
2026-01-13 21:58:50 +0100

    Refactor key mappings for improved usability

--- lua/joacoguzman/remap.lua
+++ lua/joacoguzman/remap.lua
@@ -1,26 +1,43 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = " "                                   -- leader is Space
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- File explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)            -- open netrw (:Ex)
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")             -- move selection down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")             -- move selection up
 
--- greatest remap ever
--- paste yanked text, no buffer overwrite
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Keep cursor centred when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")                  -- half-page down + centre
+vim.keymap.set("n", "<C-u>", "<C-u>zz")                  -- half-page up + centre
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
+-- Paste without overwriting yank
+vim.keymap.set("x", "<leader>p", [["_dP]])               -- paste over selection, keep yank
 
-vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+-- System clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])         -- yank to system clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]])                -- yank whole line to clipboard
+vim.keymap.set("n", "<leader>P", [["+p]])                -- paste from clipboard (normal)
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- Delete without yanking
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])         -- delete to black-hole register
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- Insert mode convenience
+vim.keymap.set("i", "<C-c>", "<Esc>")                    -- exit insert mode
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("x", "<leader>c", ":s/^/\\{\\/\\*/ | s/$/\\*\\/\\}<CR>gv")
+-- LSP
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)     -- format file via LSP
+
+-- Search & replace
+vim.keymap.set(
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
+)                                                        -- substitute word under cursor
+
+-- Comment block (useful for JSX/TSX)
+vim.keymap.set(
+  "x",
+  "<leader>c",
+  ":s/^/\\{\\/\\*/ | s/$/\\*\\/\\}<CR>gv"
+)                                                        -- wrap selection in {/* */}
