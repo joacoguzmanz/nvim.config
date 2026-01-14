4d4b2af6266b8c04a3dc34bae6a010601b841925
Joaco Guzman <joaquingusman77@gmail.com>
2026-01-14 01:24:32 +0100

    Refactor LSP configuration and dependencies

--- lua/joacoguzman/lazy/lsp.lua
+++ lua/joacoguzman/lazy/lsp.lua
@@ -1,102 +1,97 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
+    -- LSP install/bridge
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
+
+    -- Completion
+    "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+
+    -- LSP UI
     "j-hui/fidget.nvim",
   },
 
   config = function()
-    local cmp = require('cmp')
+    local cmp = require("cmp")
     local cmp_lsp = require("cmp_nvim_lsp")
+
     local capabilities = vim.tbl_deep_extend(
       "force",
       {},
       vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities())
+      cmp_lsp.default_capabilities()
+    )
 
     require("fidget").setup({})
     require("mason").setup()
+
     require("mason-lspconfig").setup({
       ensure_installed = {
         "lua_ls",
-        "ts_ls",
+        "tsserver",
         "tailwindcss",
-        "somesass_ls",
-        "pyright",
-        "astro",
-        "ruby_lsp",
         "intelephense",
+        "ruby_lsp",
+        "clangd",
       },
       handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities
-          }
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
         end,
 
-        zls = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.zls.setup({
-            root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-            settings = {
-              zls = {
-                enable_inlay_hints = true,
-                enable_snippets = true,
-                warn_style = true,
-              },
-            },
-          })
-          vim.g.zig_fmt_parse_errors = 0
-          vim.g.zig_fmt_autosave = 0
-        end,
         ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup {
+          require("lspconfig").lua_ls.setup({
             capabilities = capabilities,
             settings = {
               Lua = {
                 runtime = { version = "Lua 5.1" },
                 diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                }
-              }
-            }
-          }
-        end,
-        ["intelephense"] = function()
-          local lspconfig = require("lspconfig")
-          local util = require("lspconfig.util")
-
-          lspconfig.intelephense.setup({
-            capabilities = capabilities,
-            root_dir = util.root_pattern("wp-config.php", "composer.json", ".git", "wp-content"),
-            settings = {
-              intelephense = {
-                stubs = {
-                  "wordpress",
-                  "core",
-                  "json",
-                  "mysqli",
-                  "curl",
+                  globals = { "vim", "it", "describe", "before_each", "after_each" },
                 },
               },
             },
           })
         end,
-      }
-    })
 
-    local lspconfig = require("lspconfig")
-    lspconfig.ruby_lsp.setup({
-      capabilities = capabilities
+        ["tailwindcss"] = function()
+          require("lspconfig").tailwindcss.setup({
+            capabilities = capabilities,
+            filetypes = {
+              "html",
+              "css",
+              "scss",
+              "javascript",
+              "javascriptreact",
+              "typescript",
+              "typescriptreact",
+              "vue",
+              "svelte",
+              "heex",
+            },
+          })
+        end,
+
+        ["intelephense"] = function()
+          local util = require("lspconfig.util")
+          require("lspconfig").intelephense.setup({
+            capabilities = capabilities,
+            root_dir = util.root_pattern("wp-config.php", "composer.json", ".git", "wp-content"),
+            settings = {
+              intelephense = {
+                stubs = { "wordpress", "core", "json", "mysqli", "curl" },
+              },
+            },
+          })
+        end,
+      },
     })
 
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -104,25 +99,25 @@ return {
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
       }, {
-        { name = 'buffer' },
-      })
+        { name = "buffer" },
+      }),
     })
 
     vim.diagnostic.config({
-      -- update_in_insert = true,
       float = {
         focusable = false,
         style = "minimal",
@@ -132,5 +127,5 @@ return {
         prefix = "",
       },
     })
-  end
+  end,
 }
